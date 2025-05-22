a883d421df924464bc36902df9fcfb6504811c9b
Dev Patel <dev.patel@berkeley.edu>
2025-05-22 00:25:29 -0400

    basic microml backpropogration for mini scalar gradients

---
+++ prime.hpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <memory>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <unordered_set>
+#pragma once
+
+using namespace std;
+class Value;
+using ValuePtr = std::shared_ptr<Value>;
+
+class Value : public std::enable_shared_from_this<Value> {
+private:
+    inline static size_t currentID = 0;
+    float data;
+    float grad;
+    std::string op;
+    size_t id;
+    std::vector<ValuePtr> prev;
+    // void run_backward(vector<ValuePtr>& topo);
+    void topo_sort(vector<ValuePtr>& topo, unordered_set<Value*>& visited);
+
+public:
+    Value(float data, const std::string &op, size_t id);
+    ~Value();
+
+    static ValuePtr create(float data, const std::string& op = "");
+
+    void print(bool verbose = true, int depth = 0);
+
+    float get_val();
+    void set_prev(const vector<ValuePtr>& parents);
+    void set_grad(float grad);
+    std::string get_op();
+
+    float get_grad();
+    void add_grad(float g);
+
+
+    static ValuePtr add(const ValuePtr& lhs, const ValuePtr& rhs);
+    static ValuePtr sub(const ValuePtr& lhs, const ValuePtr& rhs);
+    static ValuePtr mult(const ValuePtr& lhs, const ValuePtr& rhs);
+    function<void()> _backward;
+    void backward();
+};
